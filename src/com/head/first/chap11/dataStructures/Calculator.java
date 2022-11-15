c2a67d3cf6d0daf170f69afa40b84a1ea3b3fe88
Tobias Gretler <tobias.gretler@axonivy.com>
2022-11-15 09:43:56 +0100

    chapter11 examples

--- src/com/head/first/chap11/dataStructures/Calculator.java
+++ src/com/head/first/chap11/dataStructures/Calculator.java
@@ -11,8 +11,9 @@ public class Calculator {
 		System.out.println("Geben sie Ihre Rechnung ein");
 
 		calc.InputNumber();
-
-		System.out.println(calc.result);
+		
+		
+		// System.out.println(calc.result);
 
 	}
 
@@ -32,45 +33,45 @@ class calculator {
 		} else if (Operator.equals("*")) {
 			multiply(Number1, Number2);
 
-		} else if (Operator.equals("/")) {
+		} else if (Operator.equals("/") || Operator.equals(":")) {
 			divide(Number1, Number2);
 
 		} else {
-			if (Operator != "+" && Operator !=  "-" && Operator !=  "*" && Operator !=  "/") {
+			if (Operator != "+" && Operator != "-" && Operator != "*" && Operator != "/" && Operator != ":") {
 				System.out.println("Geben sie ein Gültiges zeichen ein");
-				InputNumber();
-			}else  {
+				nextCalculate(result);
+			} else {
 				System.out.println("Geben sie eine Gerade ganz zahl ein.");
-				InputNumber();
+				nextCalculate(result);
 			}
 		}
-			
-		}
 
-	
+	}
 
 	public void InputNumber() {
 		Scanner operator = new Scanner(System.in);
 		int Number1 = operator.nextInt();
 		String Operator = operator.next();
 		int Number2 = operator.nextInt();
-		whichOperator(Operator, Number1, Number2);
+		
+		whichOperator(Operator,Number1, Number2);
 	}
-
+	
 	public void nextCalculate(int result) {
 		Scanner one = new Scanner(System.in);
 		String Operator = one.next();
 		int Number3 = one.nextInt();
 		whichOperator(Operator, result, Number3);
-		result = result + Number3;
-		System.out.println(result);
+		//result = result + Number3;
+		System.out.println("wieso löschen?");
+		
 	}
 
 	public int Add(int Number1, int Number2) {
 		result = Number1 + Number2;
 		System.out.println(result);
 		do {
-			System.out.println("Geben sie eine Weitere Zahl ein ein");
+			System.out.println("Geben sie eine Weitere Zahl ein ");
 			nextCalculate(result);
 
 		} while (true);
@@ -81,7 +82,7 @@ class calculator {
 		result = Number1 - Number2;
 		System.out.println(result);
 		do {
-			System.out.println("Geben sie eine Weitere Zahl ein ein");
+			System.out.println("Geben sie eine Weitere Zahl ein ");
 			nextCalculate(result);
 
 		} while (true);
@@ -91,20 +92,27 @@ class calculator {
 		result = Number1 * Number2;
 		System.out.println(result);
 		do {
-			System.out.println("Geben sie eine Weitere Zahl ein ein");
+			System.out.println("Geben sie eine Weitere Zahl ein ");
 			nextCalculate(result);
 
 		} while (true);
 	}
 
-	public int divide(int Number1, int Number2) {
+	public void divide(int Number1, int Number2) {
 		result = Number1 / Number2;
 		System.out.println(result);
 		do {
-			System.out.println("Geben sie eine Weitere Zahl ein ein");
-			nextCalculate(result);
+			System.out.println("Geben sie eine Weitere Zahl ein errerer");
+			Scanner one = new Scanner(System.in);
+			String exit = one.nextLine();
+			if (exit.equals("e")) {
+				break;
+			} else {
+				nextCalculate(result);
+			}
 
 		} while (true);
+
 	}
 
 }
