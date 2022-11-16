5f745a958fcf9e566b27e52457357a8cdc8524ae
Tobias Gretler <tobias.gretler@axonivy.com>
2022-11-16 12:23:09 +0100

    chapter11 examples

--- src/com/head/first/chap11/dataStructures/Calculator.java
+++ src/com/head/first/chap11/dataStructures/Calculator.java
@@ -3,7 +3,6 @@ package com.head.first.chap11.dataStructures;
 import java.util.Scanner;
 
 public class Calculator {
-	
 
 	public static void main(String[] args) {
 		calculator calc = new calculator();
@@ -14,20 +13,36 @@ public class Calculator {
 
 		calc.InputNumber();
 
-		
 		do {
 			System.out.println(calc.result);
-			System.out.println("Geben sie eine Weitere Zahl ein ");
-			calc.nextCalculate(calc.result);
-
+			Scanner two = new Scanner(System.in);
+			
+			// System.out.println("Geben sie eine Weitere Zahl ein ");
+			try {
+				String Operator = two.next();
+				int Number3 = two.nextInt();
+				if (Operator.equals("e")) {
+					System.out.println("Vielen Dank für das Benutzen des Rechners");
+					
+				}else {
+					calc.whichOperator(Operator, calc.result, Number3);
+				}
+				
+			} catch (Exception e) {
+				System.out.println("Geben sie eine Gültige Zahl ein");
+				calc.nextCalculate(calc.result);
+			}
+			
 		} while (true);
-
+		
+		
 	}
 
 }
 
 class calculator {
 	public int result;
+	Scanner one = new Scanner(System.in);
 
 	public void whichOperator(String Operator, int Number1, int Number2) {
 
@@ -43,34 +58,33 @@ class calculator {
 		} else if (Operator.equals("/") || Operator.equals(":")) {
 			divide(Number1, Number2);
 
+		} else if (Operator.equals("%")) {
+			percent(Number1, Number2);
+
 		} else {
-			if (Operator != "+" && Operator != "-" && Operator != "*" && Operator != "/" && Operator != ":") {
-				System.out.println("Geben sie ein Gültiges zeichen ein");
-				nextCalculate(result);
-			} else {
-				System.out.println("Geben sie eine Gerade ganz zahl ein.");
-				nextCalculate(result);
-			}
+			System.out.println("Geben sie ein Gültiges zeichen ein");
+			nextCalculate(result);
 		}
 
 	}
 
 	public void InputNumber() {
-		Scanner operator = new Scanner(System.in);
-		int Number1 = operator.nextInt();
-		String Operator = operator.next();
-		int Number2 = operator.nextInt();
+		int Number1 = 0;
+		int Number2 = 0;
+		try {
+			Number1 = one.nextInt();
+			String Operator = one.next();
+			Number2 = one.nextInt();
+			whichOperator(Operator, Number1, Number2);
+		} catch (Exception e) {
+			System.out.println("Geben sie eine Gültige Zahl ein");
+			InputNumber();
+		}
 
-		whichOperator(Operator, Number1, Number2);
 	}
 
 	public void nextCalculate(int result) {
-
-		Scanner one = new Scanner(System.in);
-		String Operator = one.next();
-		int Number3 = one.nextInt();
-		whichOperator(Operator, result, Number3);
-
+		System.out.println(" hallo ich bin nextcalculate");
 	}
 
 	public int Add(int Number1, int Number2) {
@@ -93,6 +107,11 @@ class calculator {
 		result = Number1 / Number2;
 		return result;
 
+	}
+
+	public int percent(int Number1, int Number2) {
+		result = Number2 / 100 * Number1;
+		return result;
 	}
 
 }
